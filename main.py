e9831fee824110136b9e29fa5a86e30755efbccb
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-20 15:28:24 +0300

    Add app widgets

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
-st.title("Weather Forecast App for the next days")
+st.title("Weather :green[_Forecast_] App for the next days", anchor=False)
+
+place = st.text_input("" \
+    "Enter a place to get the weather forecast",
+    help="Enter a city name to get the weather forecast")
+
+days = st.slider(
+    "Select number of days", 
+    min_value=1, 
+    max_value=5, 
+    value=1,
+    help="Select the number of days for the weather forecast")
+
+option = st.selectbox(
+    "Select data to show",
+      ["Temperature", "Sky"],
+      help="Select the type of weather data to display")
+
+st.subheader(f"{option} for {place} for the next {days} days")
