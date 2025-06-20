c313ef4a391746b6dcd0e7ff61f4a64c40fff79d
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-20 15:41:28 +0300

    Plot fake chart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+
 
 st.title("Weather :green[_Forecast_] App for the next days", anchor=False)
 
@@ -19,3 +21,16 @@ option = st.selectbox(
       help="Select the type of weather data to display")
 
 st.subheader(f"{option} for {place} for the next {days} days")
+
+
+def get_data(days):
+    dates = ["2023-10-01", "2023-10-02", "2023-10-03", "2023-10-04", "2023-10-05"]
+    temperatures = [20, 22, 19, 21, 23]
+
+    return (dates, temperatures)
+
+d,t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={'x': 'Date', 'y': 'Temperature (°C)'})
+
+st.plotly_chart(figure, use_container_width=True)
